05417030f60269cbb81856a41d9ff53ecafa0052
Ibullayev Zhanibek <ibullayevzhanibek@gmail.com>
2024-06-05 01:45:18 +0500

    Prepare project. Delete all templates comments

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -4,7 +4,7 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
